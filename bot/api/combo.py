de962ff953c3bc980608025b45e327bd18515e07
fishki <kohutiv@gmail.com>
2024-06-25 12:41:24 +0300

    init

---
+++ bot/api/combo.py
@@ -0,0 +1,22 @@
+import aiohttp
+from bot.api.http import make_post_request
+
+
+async def get_combo_cards(http_client: aiohttp.ClientSession) -> dict:
+    return await make_post_request(
+        http_client,
+        # 'https://hamster-kombo-server.vercel.app/api/GetCombo',
+        'https://12baliv.com.ua/hamster.php',
+        {},
+        'getting Combo Cards',
+    )
+
+
+async def claim_daily_combo(http_client: aiohttp.ClientSession) -> bool:
+    response_json = await make_post_request(
+        http_client,
+        'https://api.hamsterkombat.io/clicker/claim-daily-combo',
+        {},
+        'Claim Daily Combo',
+    )
+    return bool(response_json)
